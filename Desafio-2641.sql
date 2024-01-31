2f4346d40c4fa3a0e00dd372891268b77811c31b
Will <will.oliveira1@hotmail.com>
2024-01-31 18:10:36 -0300

    feat - Desafios BeeCrowd

---
+++ Desafio-2641.sql
@@ -0,0 +1,22 @@
+-- Link Desafio:  https://www.beecrowd.com.br/judge/pt/problems/view/2641
+-- Link Resposta: https://www.beecrowd.com.br/judge/pt/runs/code/37407344
+
+-- Resposta
+SELECT 'Approved: ' || name, grade
+FROM students
+WHERE grade >= 7
+ORDER BY grade DESC;
+
+-- Dados Iniciais
+CREATE TABLE students(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    grade NUMERIC
+);
+INSERT INTO students(id, name, grade)
+VALUES (1, 'Terry B. Padilla',7.3),
+       (2, 'William S. Ray',0.6),
+       (3, 'Barbara A. Gongora',5.2),
+       (4, 'Julie B. Manzer', 6.7),
+       (5, 'Teresa J. Axtell', 4.6),
+       (6, 'Ben M. Dantzler', 9.6);
